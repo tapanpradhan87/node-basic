e8d8f1f05ac45707429f5afb8b2e9860e004b565
tapanpradhan87 <tkpradhan87@gmail.com>
2024-03-25 22:43:20 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+const readFile = (path) => {
+    return new Promise((onSuccess, onFailure) => {
+        fs.readFile(path, 'utf-8', (error, data) => {
+            if (error) onFailure(error);
+            onSuccess(data);
+        });
+    })
+}
+
+const initApp = async () => {
+    const data = await readFile('src/tests/test.txt');
+    console.log(data);
+    console.log('Hello World')
+}
+
+initApp();
